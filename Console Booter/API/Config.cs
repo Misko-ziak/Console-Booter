1396ed4b8e60d0f2c5029690cad574b3e1e43e8b
Night <nightbooter@gmail.com>
2018-09-12 19:32:34 -0300

    Update Config.cs

--- Console Booter/API/Config.cs
+++ Console Booter/API/Config.cs
@@ -11,7 +11,7 @@ namespace Console_Booter.API
         //This Program as Maked By Night
         //Please Don't remove this commentary
         public static string BooterName = "C# Console Booter / Completo!";
-        public static string[] APIs = new string[] { "http://testphp.vulnweb.com", "http://vulnweb.com" };
+        public static string[] APIs = new string[] { "http://websiteexample_.com", "http://websiteexample_2.com" };
         public static string[] HelpText = new string[] {"-atk / --atack {ip} {port} {time}","-c / --credits, Just Show Some informations."};
         public static string[] InfoAbout = new string[] { "Program Maked by YourName", "LineAboutYourBooter", "LineAboutYourBooter-2" };
     }
